f1be283a4fcfe4eb0750e4087af204d9e63509c3
totan68 <164209696+totan68@users.noreply.github.com>
2024-08-27 09:39:22 +0530

    Initial Commit

---
+++ newprojectdemogithub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newprojectdemogithub
+//
+//  Created by Totan Hazra on 27/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newprojectdemogithub/newprojectdemogithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  newprojectdemogithubApp.swift
+//  newprojectdemogithub
+//
+//  Created by Totan Hazra on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct newprojectdemogithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
